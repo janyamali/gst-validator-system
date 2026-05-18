b35abe111f3ac99204c86ebf4948d9bb213225bf
janyamali <janyamali@outlook.com>
2026-05-18 13:28:52 +0530

    Fix get_db import for Render

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,17 +1,11 @@
-import os
-
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from os import getenv
 
+DATABASE_URL = getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL"
-)
-
-engine = create_engine(
-    DATABASE_URL
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -20,3 +14,13 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
